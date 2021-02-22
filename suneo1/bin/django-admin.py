427a4d220e4e4240a6fdd12ac7e6133c396a5c51
sunilkumarchoudari <chatrapathissunilkumar0@gmail.com>
2021-02-22 17:59:52 +0530

    first commit

---
+++ suneo1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nbh/projects/Student_Result_Management_System_using_Django/SRMS/suneo1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
