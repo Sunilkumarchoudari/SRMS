9cfd2581529d0adb227734d00914fd01dbcd50d9
sunilkumarchoudari <chatrapathissunilkumar0@gmail.com>
2021-04-01 11:04:53 +0530

    VENV removed

--- suneo1/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/nbh/projects/Student_Result_Management_System_using_Django/SRMS/suneo1/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
